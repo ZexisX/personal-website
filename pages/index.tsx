efdef31b3532ab2b7cca3562e611a43dc5295ada
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-12-24 11:52:41 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,9 +22,9 @@ export default function MyComponent() {
         isVisible || !animationFinished ? 'visible' : 'hidden'
       }`}
     >
-      <h1 className="text-6xl font-bold mb-2 bg-gradient-to-r from-gray-400 via-gray-500 to-white text-transparent bg-clip-text">
-        Le Vinh Khang
-      </h1>
+      <h1 className="text-6xl font-bold mb-2 bg-gradient-to-r from-gray-400 via-gray-500 to-white text-transparent bg-clip-text text-center">
+  Le Vinh Khang
+</h1>
       <h2 className="text-4xl font-light mb-4 bg-gradient-to-r from-white via-gray-500 to-gray-900 text-transparent bg-clip-text">
         Developer
       </h2>
