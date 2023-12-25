b83d85a1f4ce062c4c8609cca903466affb3098b
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-12-25 19:38:27 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function MyComponent() {
         isVisible || !animationFinished ? 'visible' : 'hidden'
       }`}
     >
-      <h1 className="text-6xl font-bold mb-2 bg-gradient-to-r from-gray-400 via-gray-500 to-white text-transparent bg-clip-text text-center">
+      <h1 className="text-6xl font-bold mb-2 gradient-text text-transparent bg-clip-text text-center">
         {name}
       </h1>
       <h2 className="text-4xl font-light mb-4 bg-gradient-to-r from-white via-gray-500 to-gray-900 text-transparent bg-clip-text">
